802c58ac0d9086c767751e768dc74cfa31d27a4f
Jakub Maslowski <jakub@Foundas-MBP.home>
2021-01-02 17:14:59 +0100

    basic router setup and navbar functionality

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/styles'
 import { Theme } from '@material-ui/core/styles/createMuiTheme'
 import { Typography } from '@material-ui/core'
+
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+// import Tabs from '@material-ui/core/Tabs'
+// import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
+
+interface Tab {
+  to: string
+  label: string
+  active: boolean
+}
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -30,8 +39,11 @@ const useStyles = makeStyles((theme: Theme) => {
       fontSize: '0.7rem',
       lineHeight: '1'
     },
-    tabsContainer: {
-      width: '100%'
+    logoContainer: {
+      padding: 0,
+      '&:hover': {
+        backgroundColor: 'transparent'
+      }
     },
     tab: {
       textTransform: 'none',
@@ -46,12 +58,88 @@ const useStyles = makeStyles((theme: Theme) => {
       width: '90%',
       margin: '0 auto',
       borderRadius: '200px'
+    },
+    tabsContainer: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-evenly'
+    },
+    tabActive: {
+      border: '1px solid #707070',
+      width: '120%'
+    },
+    tabDisactive: {
+      border: '1px solid transparent',
+      width: '120%'
+    },
+    tabButtonContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      boxSizing: 'border-box'
     }
   }
 })
 
-export default function ElevateAppBar() {
+export default function Header() {
   const classes = useStyles()
+  const [tab, setTab] = useState<Tab[]>([
+    {
+      to: '/work',
+      label: 'Work',
+      active: false
+    },
+    {
+      to: '/contact',
+      label: 'Contact',
+      active: false
+    },
+    {
+      to: '/about',
+      label: 'About',
+      active: false
+    }
+  ])
+
+  const handleChange = (index: number, activeTab: Tab) => {
+    let newArray = [...tab]
+    newArray.forEach((element) => {
+      element.active = false
+    })
+    newArray[index] = { ...activeTab, active: true }
+    setTab(newArray)
+  }
+
+  useEffect(() => {
+    console.log(window.location.pathname)
+    let newArray = [...tab]
+    newArray.forEach((element) => {
+      if (element.to === window.location.pathname) element.active = true
+    })
+    setTab(newArray)
+  }, [])
+
+  const renderTabs = (tabs: Tab[]) => {
+    return tabs.map((tab, i) => (
+      <div className={classes.tabButtonContainer} key={tab.label}>
+        <Button
+          className={classes.tab}
+          component={Link}
+          to={tab.to}
+          disableRipple
+          onClick={() => handleChange(i, tab)}
+        >
+          {tab.label}
+        </Button>
+        {
+          <div
+            className={tab.active ? classes.tabActive : classes.tabDisactive}
+          />
+        }
+      </div>
+    ))
+  }
+
   return (
     <React.Fragment>
       <AppBar
@@ -60,19 +148,26 @@ export default function ElevateAppBar() {
         className={classes.headerContainer}
       >
         <Toolbar disableGutters className={classes.toolbarContainer}>
-          <div>
+          <Button
+            component={Link}
+            to="/"
+            disableRipple
+            className={classes.logoContainer}
+            onClick={() =>
+              handleChange(-1, {
+                to: '/',
+                label: '',
+                active: false
+              })
+            }
+          >
             <Typography component="p" className={classes.logo}>
               Rudolf
-            </Typography>
-            <Typography component="p" className={classes.logo}>
+              <br />
               Maslowski
             </Typography>
-          </div>
-          <Tabs className={classes.tabsContainer}>
-            <Tab className={classes.tab} label="Work" />
-            <Tab className={classes.tab} label="About" />
-            <Tab className={classes.tab} label="Contact" />
-          </Tabs>
+          </Button>
+          <div className={classes.tabsContainer}>{renderTabs(tab)}</div>
           <Typography component="p" className={classes.languageToggler}>
             PL/<strong>EN</strong>
           </Typography>
